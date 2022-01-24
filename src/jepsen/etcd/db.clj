17a2323a77c0c124f68c63a4d6fa17179141923d
mengrj <mengrj.cs@gamil.com>
2022-01-24 22:22:52 +0800

    change links

--- src/jepsen/etcd/db.clj
+++ src/jepsen/etcd/db.clj
@@ -202,9 +202,9 @@
       (let [version (:version test)]
         (info node "installing etcd" version)
         (c/su
-          (let [url (str "https://github.com/mengrj/etcd/tree/version/package/v" version
+          (let [url (str "https://github.com/mengrj/etcd/raw/version/package/v" version
                         "/etcd-v" version "-linux-amd64.tar.gz")]
-          ;(let [url (str "https://github.com/mengrj/etcd/blob/version/package/etcd-modify-linux-amd64.tar.gz")]
+          ;(let [url (str "https://github.com/mengrj/etcd/raw/version/package/etcd-modify-linux-amd64.tar.gz")]
             (cu/install-archive! url dir))))
       (db/start! db test node)
 
